61e36b3c520901edb486d0e5c0b45b6b094062a0
harushin <abc@hogehoge.com>
2026-01-27 21:26:23 +0900

    20260127 sql周りの検証準備+lostupdateのdockerイメージの準備

---
+++ sql_lab/lost_update/init/ddl.sql
@@ -0,0 +1,28 @@
+/*
+データベースの初期化
+*/
+
+-- DB作成（公式のイメージ位をPULLしているためか、compose.yml内の設定を見てtraining_dbが作成されている？）
+-- CREATE DATABASE training_db;
+
+-- DBへの接続
+\c training_db;
+
+-- テーブル作成
+DROP TABLE IF EXISTS sample_tbl;
+CREATE TABLE sample_tbl (
+  id CHAR(15) NOT NULL PRIMARY KEY
+  , balance INTEGER NOT NULL
+  , created_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- ID用シーケンスの作成
+CREATE SEQUENCE sample_id_seq START 1;
+
+-- サンプルデータの登録
+INSERT INTO sample_tbl
+SELECT 
+    LEFT(MD5(i::text), 8) || '_' || LPAD(nextval('sample_id_seq')::TEXT, 6, '0')
+    , FLOOR(RANDOM()*10000 + 100)
+    , CURRENT_DATE + (random() * 365)::int * INTERVAL '1 day'
+FROM generate_series(1, 100000) s(i);
