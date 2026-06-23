65e59c2554459f73cf0d561e4d551be91cd4d6e8
harushin <abc@hogehoge.com>
2026-06-23 16:12:52 +0900

    20260623 SQLアンチパターン14章-25章

--- sql_lab/antipattern/1_データベース論理設計/4_ID-Required/id-required.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE ArticleTags (
-  id          SERIAL PRIMARY KEY,
-  article_id  BIGINT NOT NULL,
-  tag_id      BIGINT NOT NULL,
-  FOREIGN KEY (article_id) REFERENCES Articles (id),
-  FOREIGN KEY (tag_id)     REFERENCES Tags (id)
-);
--- sql_lab/antipattern/1_データベース論理設計/5_Keyless-Entry/keyless-entry.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Bugs (
-  -- 他の列 . . .
-  reported_by       BIGINT NOT NULL,
-  status            VARCHAR(20) NOT NULL DEFAULT 'NEW',
-  FOREIGN KEY (reported_by) REFERENCES Accounts(account_id),
-  FOREIGN KEY (status) REFERENCES BugStatus(status)
-);
